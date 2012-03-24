b09fb8954bdd7b1942f7d6cfdaba9e28009c22b9
Walter Semelshtein <walter@pictomotion.com>
2012-03-23 21:13:06 -0300

    javascript error on image fails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -97,42 +97,37 @@ function c_carousel(){
   );
 }
 
-
+function onImgError(){
+	console.log("error")
+}
+ 
 // http://net.tutsplus.com/tutorials/javascript-ajax/how-to-transition-an-image-from-bw-to-color-with-canvas/
 function grayscale(src) {
-
-	var imgcheck = src.width;
-
-	if (imgcheck== undefined) {
-
-	}else{
-
-	    var supportsCanvas = !!document.createElement('canvas').getContext;
-	    if (supportsCanvas) {
-	        var canvas = document.createElement('canvas'),
-	        context = canvas.getContext('2d'),
-	        imageData, px, length, i = 0, gray,
-	        img = new Image();
-	 
-	        img.src = src;
-	        canvas.width = img.width;
-	        canvas.height = img.height;
-	        context.drawImage(img, 0, 0);
-	 
-	        imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-	        px = imageData.data;
-	        length = px.length;
-	 
-	        for (; i < length; i += 4) {
-	            gray = px[i] * .3 + px[i + 1] * .59 + px[i + 2] * .11;
-	            px[i] = px[i + 1] = px[i + 2] = gray;
-	        }
-	 
-	        context.putImageData(imageData, 0, 0);
-	        return canvas.toDataURL();
-	    } else {
-	        return src;
-	    }
-
-	}
+	console.log(src)
+    var supportsCanvas = !!document.createElement('canvas').getContext;
+    if (supportsCanvas) {
+        var canvas = document.createElement('canvas'),
+        context = canvas.getContext('2d'),
+        imageData, px, length, i = 0, gray,
+        img = new Image();
+ 
+        img.src = src;
+        canvas.width = img.width;
+        canvas.height = img.height;
+        context.drawImage(img, 0, 0);
+ 
+        imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+        px = imageData.data;
+        length = px.length;
+ 
+        for (; i < length; i += 4) {
+            gray = px[i] * .3 + px[i + 1] * .59 + px[i + 2] * .11;
+            px[i] = px[i + 1] = px[i + 2] = gray;
+        }
+ 
+        context.putImageData(imageData, 0, 0);
+        return canvas.toDataURL();
+    } else {
+        return src;
+    }
 }
